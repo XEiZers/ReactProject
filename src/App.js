6547b15abdc2894f9ac19c29a1c10b71ca8e805e
XEiZers <benanikin@gmail.com>
2023-02-25 15:22:01 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
-import React from "react";
-import logo from './logo.svg';
+import React, { Component } from "react";
+
+import {Navbar,Nav,FormControl,Container,Form,Button
+} from "react-bootstrap";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-function App() {
-  return (
-      <div>
-          <h1> Hello world!</h1>
-          <Button variant="primary">Button</Button>
-      </div>
-  );
+import logo from "./logo192.png";
+export default class App extends Component {
+    render() {
+        return (
+            <>
+                <Navbar fixed="top" collapseOnSelect expand="md" bg="dark" variant="dark">
+                    <Container>
+                        <Navbar.Brand href="/">
+                            <img
+                                src={logo}
+                                height="30"
+                                width="30"
+                                className="d-inline-block align-top"
+                                alt = "Logo"
+                            /> React Site
+                        </Navbar.Brand>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                            <Nav claccName="me-auto">
+                                <Nav.Link href="/"> Home
+                                </Nav.Link> <Nav.Link href="/about"> About us
+                            </Nav.Link> <Nav.Link href="/contacts"> Contacts </Nav.Link>
+                                <Nav.Link href="/blog"> Blog </Nav.Link>
+                            </Nav>
+                            <Form className="d-flex">
+                                <FormControl
+                                    type ="text"
+                                    placeholder="Search"
+                                    className="me-sm-3"
+                                />
+                                <Button variant="outline-info">Search</Button>
+                            </Form>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </>
+        )
+    }
 }
-
-export default App;
