3277a20ae40e81bee7f1bc18ea84a969ef67d711
XEiZers <benanikin@gmail.com>
2023-02-25 15:59:22 +0200

    part5

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
 import {Navbar,Nav,FormControl,Container,Form,Button
 } from "react-bootstrap";
-
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import logo from "./logo192.jpg";
+import Blog from "./Pages/Blog";
+import Contacts from "./Pages/Contacts";
+import About from "./Pages/About";
+
+function Home() {
+    return null;
+}
+
 export default class App extends Component {
     render() {
         return (
@@ -21,10 +29,10 @@ export default class App extends Component {
                         <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
                         <Navbar.Collapse id="responsive-navbar-nav">
                             <Nav claccName="me-auto">
-                                <Nav.Link href="/Pages/Home"> Home</Nav.Link>
-                                <Nav.Link href="/Pages/About"> About us</Nav.Link>
-                                <Nav.Link href="/Pages/Contacts"> Contacts </Nav.Link>
-                                <Nav.Link href="/Pages/Blog"> Blog </Nav.Link>
+                                <Nav.Link href="/Home"> Home</Nav.Link>
+                                <Nav.Link href="/About"> About us</Nav.Link>
+                                <Nav.Link href="/Contacts"> Contacts </Nav.Link>
+                                <Nav.Link href="/Blog"> Blog </Nav.Link>
                             </Nav>
                             <Form className="d-flex">
                                 <FormControl
@@ -37,6 +45,16 @@ export default class App extends Component {
                         </Navbar.Collapse>
                     </Container>
                 </Navbar>
+
+                <Router>
+                    <Routes>
+                        <Route path="/Home" element={<Home />}/>
+                        <Route path="/About" element={<About/>}/>
+                        <Route path="/Contacts" element={<Contacts/>}/>
+                        <Route path="/Blog" element={<Blog/>}/>
+                    </Routes>
+                </Router>
+
             </>
         )
     }
