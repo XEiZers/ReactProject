6d5a5704c3096650e2712bd2f37a5a2445fb4960
XEiZers <benanikin@gmail.com>
2023-02-26 11:26:01 +0200

    part10

--- src/Components/CarouselBoxHk.js
+++ src/Components/CarouselBoxHk.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
-import MushroomsImg from '../assets/pexels-visually.jpg';
-import OceanImg from '../assets/pexels-stephane.jpg';
+import WatImg from '../assets/&&.png';
+import Num27Img from '../assets/NC_27.svg.png';
+import Num91Img from '../assets/US_91.svg.png';
+
 import {Carousel} from "react-bootstrap";
 class CarouselBoxHk extends Component{
 
@@ -8,16 +10,25 @@ render() {
     return (
         <Carousel>
             <Carousel.Item>
-                <img className="d-block w-100" src={OceanImg} alt="Ocean"/>
+                <img className="d-block w-100" src={Num27Img} alt="Num27Img"/>
                 <Carousel.Caption>
-                    <h3>Ocean image</h3>
+                    <h3>Num27Img</h3>
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem, est?</p>
                 </Carousel.Caption>
             </Carousel.Item>
+
             <Carousel.Item>
-                <img className="d-block w-100" src={MushroomsImg} alt="Mushrooms"/>
+                <img className="d-block w-100" src={Num91Img} alt="Num91Img"/>
                 <Carousel.Caption>
-                    <h3>Mushrooms image</h3>
+                    <h3>Num91Img</h3>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem, est?</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item>
+                <img className="d-block w-100" src={WatImg} alt="WatImg"/>
+                <Carousel.Caption>
+                    <h3>WatImg</h3>
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem, est?</p>
                 </Carousel.Caption>
             </Carousel.Item>
